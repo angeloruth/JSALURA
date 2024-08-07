7d3f68e7b1e75b90453557fe16c0ad35f24e721e
angeloruth <ro081986@gmail.com>
2024-08-07 08:20:16 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 alert('Boas vindas ao numero secreto');
-let numeroSecreto = 29;
-let chute = prompt('Escolha um número entre 1 e 30');
+let numeroSecreto = 05;
+console.log(numeroSecreto)
+let chute = prompt('Escolha um número entre 1 e 10');
 
 // se chute for igual ao número secreto
 if(chute == numeroSecreto) {
     alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto}`);
 
 } else {
-    if(chute>numeroSecreto)  
+    if(chute > numeroSecreto) { 
         alert(`O número secreto é menor que ${chute}`);
 } else  {
 alert(`O número secreto é maior que ${chute}`);
